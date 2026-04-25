200236d48f57c4d52298d365acadca2d1c4162df
davidlevere@gmail.com <davidlevere@gmail.com>
2026-04-25 17:40:04 -0400

    todo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+// eat stdin and return it?
+// search upward for .bank directory indicate failure if it's not there
+// get time
+// take arg 2 if it exists 
+// are there any files in .bank? no then make one and dump the info into it
+// if files are here, then find the youngest one, if it's sufficiently small then append to it
+// finished
+//
+// ===========================
+// arg 2
+// date
+// stdin
+
+
 fn main() {
     println!("Hello, world!");
 }
