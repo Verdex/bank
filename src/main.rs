7840ca889ab038519204044938e388029db2d83e
davidlevere@gmail.com <davidlevere@gmail.com>
2026-04-26 00:18:42 -0400

    find repo

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,51 @@ fn main() {
     let input = io::read_to_string(io::stdin()).expect("reading stream failed");
     print!("{}", input); // TODO println?
 
-    find_bank();
+    let bank_dir = find_bank().expect("couldn't find .bank directory");
+
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() > 1 {
+
+    }
+
+}
+
+fn find_next_repo(mut bank : PathBuf) -> io::Result<PathBuf> {
+    fn to_time(x:PathBuf) -> (PathBuf, u64) {
+        let time = match fs::metadata(&x) {
+            Ok(m) => match m.created() {
+                Ok(t) => t.duration_since(UNIX_EPOCH).expect("time failure").as_secs(),
+                Err(_) => 0,
+            },
+            Err(_) => 0,
+        };
+        (x, time)
+    }
+
+    let dir = fs::read_dir(&bank)?;
+    let result = dir
+        .filter_map(|x| x.ok())
+        .map(|x| x.path())
+        .filter(|x| x.is_file())
+        .map(to_time)
+        .max_by_key(|(p, t)| *t);
+
+    match result { 
+        Some(x) => {
+            let m = fs::metadata(&x.0)?;
+            if m.len() <= MAX_REPO_SIZE {
+                Ok(x.0)
+            }
+            else {
+                bank.push(format!("{}", SystemTime::now().duration_since(UNIX_EPOCH).expect("time name failure").as_secs()));
+                Ok(bank)
+            }
+        },
+        None => { 
+            bank.push(format!("{}", SystemTime::now().duration_since(UNIX_EPOCH).expect("time name failure").as_secs()));
+            Ok(bank)
+        }
+    }
 }
 
 fn find_bank() {
